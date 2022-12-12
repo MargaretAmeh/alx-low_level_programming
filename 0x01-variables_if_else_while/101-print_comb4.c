bc92ffe90b34ca839eba77d971f1a2d223df52cc
MargaretAmeh <amehmargaret6@gmail.com>
2022-12-12 09:43:56 -0800

    Print Comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,54 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinatiopns of two digits
- * Numbers that are seperated by ,, followed by a space
- * The three digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the
- * same combination of the three digits 0, 1 and 2
- * Print only the smallest combination of three digits
- * Numbers should be printed in ascending order, with three digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar six times maximum in your code
- * You are not allowed to use any variable of type char
- * All your code should be in the main function
- * Return: 0
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int c;
-	int d;
-	int e = 0;
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
 
-	while (e < 10)
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		d = 0;
-		while (d < 10)
+		for (tens = '0'; tens <= '9'; tens++)
 		{
-			c = 10;
-			while (c < 10)
+			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (c != d && d != e && e < d && d < c)
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 				{
-					putchar('0' + e);
-					putchar('0' + d);
-					putchar('0' + c);
-
-					if (c + d + e != 9 + 8 + 7)
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' &&
+								tens == '8'))/*adds commas and spaces*/
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-
-				c++;
 			}
-			d++;
 		}
-		e++;
 	}
 	putchar('\n');
 	return (0);
