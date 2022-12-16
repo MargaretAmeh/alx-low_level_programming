e775a83e069d617707c8841e4133a5d811bf8f54
MargaretAmeh <amehmargaret6@gmail.com>
2022-12-16 04:11:45 -0800

    More_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,14 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (y >= 10)
+			if (y > 9)
+			{
 				_putchar((y / 10) + '0');
+			}
 			_putchar((y % 10) + '0');
 		}
+		_putchar(10);
 	}
-
-	_putchar('\n');
 }
