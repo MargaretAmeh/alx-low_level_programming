4d8d43c0d302efb3957932deecdd544be86800af
MargaretAmeh <amehmargaret6@gmail.com>
2022-12-16 03:53:18 -0800

    More_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,18 +3,19 @@
 /**
  * more_numbers -> printing more numbers
  */
+
 void more_numbers(void)
 {
 	int x, y;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y < 10; y++)
 		{
 			if (y >= 10)
-				putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
