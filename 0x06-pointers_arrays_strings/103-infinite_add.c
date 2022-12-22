3b09ea72d228366dc156a0a18deb27fc9cef49c1
MargaretAmeh <amehmargaret6@gmail.com>
2022-12-22 05:56:08 -0800

    Infinit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,6 +17,7 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	return (0);
 }
 
 /**
@@ -35,24 +36,24 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
-		num = (*n1 -'0') + (*n2 - '0');
+		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for (i *n1; n1--; r_index++)
+	for (; *n1; n1--; r_index++)
 	{
 		num = *(n1 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
 
-	for (; *n2; n2--; r_index--)
+	for (; *n2; n2--;  r_index--)
 	{
 		num = (*n2 - '0') + tens;
-		*(r + r_index) =(num % 10) + '0';
+		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
+	return (0);
 }
-
