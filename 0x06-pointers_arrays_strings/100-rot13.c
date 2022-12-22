5050d3b01ae4e69a1a436851bc1cea08963e11ea
MargaretAmeh <amehmargaret6@gmail.com>
2022-12-22 09:15:37 -0800

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,7 +12,7 @@ char *rot13(char *str)
 	int index1, index2;
 
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		             'G', 'H', 'I', 'J', 'K', 'L',
+		'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
 			     'S', 'T', 'U', 'V', 'W', 'X',
 			     'Y', 'Z', 'a', 'b', 'c', 'd',
@@ -21,7 +21,7 @@ char *rot13(char *str)
 			     'q', 'r', 's', 't', 'u', 'v',
 			     'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		             'T', 'U', 'V', 'W', 'X', 'Y',
+		'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
 			     'F', 'G', 'H', 'I', 'J', 'K',
 			     'L', 'M', 'n', 'o', 'p', 'q',
